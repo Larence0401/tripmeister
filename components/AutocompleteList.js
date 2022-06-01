bd3068488c0066c266c2953c96f148cda94ddd0e
unknown <aventuras030@gmail.com>
2022-06-01 09:55:01 +0200

    fixed bug #1

--- components/AutocompleteList.js
+++ components/AutocompleteList.js
@@ -2,15 +2,14 @@ import tw from "tailwind-styled-components";
 import { useAppContext } from "../store/appContext";
 import { useState, useEffect } from "react";
 
-
 // This component renders a list of autocomplete suggestions for locations based on the characters the user types into the input field.
-//
+// The suggestions are provided by the Mapbox GL API and stored in the global state.
 
 const AutocompleteList = ({ setStart, setEnd }) => {
   const { state } = useAppContext();
   const [val, setVal] = useState(null);
   const suggestions = setStart ? "startSuggestion" : "endSuggestion";
-  const marginLeft = state.selectedStopData ? "" : "mx-8"
+  const marginLeft = state.selectedStopData ? "" : "mx-8";
 
   useEffect(() => {
     setStart ? setStart(val) : setEnd(val);
@@ -19,20 +18,21 @@ const AutocompleteList = ({ setStart, setEnd }) => {
   return (
     <Wrapper className={marginLeft}>
       <ul>
-      {state[`${suggestions}`].map((el, index) => (
-          <ListItem
-            onClick={() =>
-              setVal({
-                place: el.place_name,
-                index: index,
-                coordinates: el.center,
-              })
-            }
-            key={index}
-          >
-            {el.place_name}
-          </ListItem>
-        ))}
+        {state[`${suggestions}`].length > 0 &&
+          state[`${suggestions}`].map((el, index) => (
+            <ListItem
+              onClick={() =>
+                setVal({
+                  place: el.place_name,
+                  index: index,
+                  coordinates: el.center,
+                })
+              }
+              key={index}
+            >
+              {el.place_name}
+            </ListItem>
+          ))}
       </ul>
     </Wrapper>
   );
--- hooks/useFetchSearchResults.js
+++ hooks/useFetchSearchResults.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { useAppContext } from "../store/appContext";
 
 const useFetchSearchResults = (input) => {
