47c09fef1944f46320c4df463702be1fa85ee9ce
unknown <aventuras030@gmail.com>
2022-06-06 18:29:59 +0200

    created .env.local folder

--- next.config.js
+++ next.config.js
@@ -3,16 +3,16 @@ const nextConfig = {
   reactStrictMode: true,
 };
 
+
 module.exports = {
   env: {
-    mapbox_key:
-      "pk.eyJ1IjoibGF3cmVuY2UwMzAiLCJhIjoiY2wxY210OGZ6MDk1YzNqbzJmNzJtNGs0NyJ9.Xh5J2xq0B_eTtvTDPWzaew",
-    apiKey: "AIzaSyCslMeCOzjhzgJlzG6mMD-kWb3nfbEDtLs",
-    authDomain: "tripmeister-663c5.firebaseapp.com",
-    projectId: "tripmeister-663c5",
-    storageBucket: "tripmeister-663c5.appspot.com",
-    messagingSenderId: "227535778820",
-    appId: "1:227535778820:web:731228e9d27ea71fde3151",
-    measurementId: "G-VQ08YBQF9Y",
+    mapbox_key: process.env.mapbox_key,
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId,
+    measurementId: process.env.measurementId,
   },
 };
